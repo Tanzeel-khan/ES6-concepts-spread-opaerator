3a09a484ecad911f40d6e4add3c6c3f4ad85196d
Tanzeel-khan <ktanzeel184@gmail.com>
2020-08-30 17:28:12 +0500

    class 23

---
+++ app.js
@@ -0,0 +1,45 @@
+// ES6 //
+
+//var//
+
+// var fname = "tanzeel";
+// function name() {
+   //     console.log(fname)
+// }
+// name()
+
+//let//
+
+// let fname = "tanzeel"
+// let fname = "khan"
+// console.log (fname);
+
+//const //
+
+// const fname = "tanzeel"
+// const fname = "khan"
+// console.log(fname)
+
+//templelate literals //
+
+// var fname = "tanzeel";
+// var lname = "khan";
+// var name = "my name is" +fname+ "my last name is "+lname
+
+// this is old js method but in es6 there is tempelate literals
+
+// let full = (`my name is ${fname}`);
+// console.log(full)
+
+// spread operators // 
+
+// var students1 = ["ali" ,"tanzeel"]
+// var students2 = [...students1,"khan","sahab"];
+// console.log(students2);
+
+var students1 ={
+    name : "ghous",
+    school : "s.m"
+}
+var students2 = {...students1,class:"five"}
+console.log(students2)
